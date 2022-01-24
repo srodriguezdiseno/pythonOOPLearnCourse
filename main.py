b9a714a1d6ce36859b4df041e9c1927c69d5e160
Samuel <srodriguezdiseno@gmail.com>
2022-01-24 10:03:53 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import turtle
+
+myturtle = turtle.Turtle()
+
+
+myturtle.penup()
+myturtle.goto(50,70)
+
+myturtle.pendown()
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+
+
+turtle.done()
